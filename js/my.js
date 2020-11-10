db8e7c2a8b802008f9f8c3180b96ffb98729fb06
skydall <hok259440@gmail.com>
2020-11-10 19:16:13 +0300

    Практическая №5  - Калькулятор

--- js/my.js
+++ js/my.js
@@ -14,3 +14,25 @@ var wow = new WOW(
     }
   );
   wow.init();
+  function square() {
+    let a = document.sq.sq_a.value;
+    let b = document.sq.sq_b.value;
+    let c = document.sq.sq_c.value;
+    let sol = document.getElementById("sq_sol");
+    let d = b * b - 4 * a * c;
+    let str;
+    if (d < 0 ) {
+      str = "Вещественных корней нет";
+    } else if (d == 0) {
+      str = "Два одинаковых вещественных корня: <br>x<sub>1</sub> = x<sub>2</sub>";
+      str += -b/(2*a);
+      str += ".";
+    } else {
+      str = "Два различных вещественных корня : <br>x<sub>1</sub>";
+      str += -b/(2*a)-Math.sqrt(d)/(2*a);
+      str += ", x<sub>2</sub> = ";
+      str += -b/(2*a)+ Math.sqrt(d)/(2*a);
+      str += ".";
+    }
+    sol.innerHTML = str;
+  }
